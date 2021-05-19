dbf001d4a6d8861f16354da274276fec92589128
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-20 05:18:53 +0530

    ADDED:To use wrapper Data type and equal method

--- src/main/java/com/bridgelabz/LineComparison.java
+++ src/main/java/com/bridgelabz/LineComparison.java
@@ -8,22 +8,29 @@ public class LineComparison {
         // Creates an object of Scanner
         Scanner input = new Scanner(System.in);
         //Given input value
-        int x1= input.nextInt();
-        int x2= input.nextInt();
-        int y1= input.nextInt();
-        int y2= input.nextInt();
+        Integer x1= input.nextInt();
+        Integer x2= input.nextInt();
+        Integer y1= input.nextInt();
+        Integer y2= input.nextInt();
 
-        int a1= input.nextInt();
-        int a2= input.nextInt();
-        int b1= input.nextInt();
-        int b2= input.nextInt();
+        Integer a1= input.nextInt();
+        Integer a2= input.nextInt();
+        Integer b1= input.nextInt();
+        Integer b2= input.nextInt();
 
         //Length of line calculation
-        double lengthOne = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        double lengthTwo = Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2));
+        Double lengthOne = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        Double lengthTwo = Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2));
 
         //Given equal operator
         if (lengthOne == lengthTwo) {
+            System.out.println("Equal ");
+        } else {
+            System.out.println("Not Equal ");
+        }
+
+        //Given equal method
+        if(lengthOne.equals(lengthTwo)){
             System.out.println("Equal ");
         } else {
             System.out.println("Not Equal ");
