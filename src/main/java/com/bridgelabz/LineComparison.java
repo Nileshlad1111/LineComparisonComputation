5ef9d459c3d78800683962f4ac5dac2478596266
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-20 05:06:02 +0530

    ADDED:TO use the wrapper data type and add the equal method and compareTo lengthOne,lengthTwo

--- src/main/java/com/bridgelabz/LineComparison.java
+++ src/main/java/com/bridgelabz/LineComparison.java
@@ -8,19 +8,19 @@ public class LineComparison {
         // Creates an object of Scanner
         Scanner input = new Scanner(System.in);
         //Given input value
-        int x1= input.nextInt();
-        int x2= input.nextInt();
-        int y1= input.nextInt();
-        int y2= input.nextInt();
+        Integer x1= input.nextInt();
+        Integer x2= input.nextInt();
+        Integer y1= input.nextInt();
+        Integer y2= input.nextInt();
 
-        int a1= input.nextInt();
-        int a2= input.nextInt();
-        int b1= input.nextInt();
-        int b2= input.nextInt();
+        Integer a1= input.nextInt();
+        Integer a2= input.nextInt();
+        Integer b1= input.nextInt();
+        Integer b2= input.nextInt();
 
         //Length of line calculation
-        double lengthOne = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        double lengthTwo = Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2));
+        Double lengthOne = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        Double lengthTwo = Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2));
 
         //Given equal operator
         if (lengthOne == lengthTwo) {
@@ -29,8 +29,15 @@ public class LineComparison {
             System.out.println("Not Equal ");
         }
 
-        //Given compare two value
-        int diffInLength = (int) (lengthOne+lengthTwo);
+        //Given equal method
+        if(lengthOne.equals(lengthTwo)){
+            System.out.println("Equal ");
+        } else {
+            System.out.println("Not Equal ");
+        }
+
+        //Given compare two value and use compareTo method
+        int diffInLength = lengthOne.compareTo(lengthTwo);
         if (diffInLength == 0) {
             System.out.println("Equal of line length");
         } else if (diffInLength > 0) {
