cd6e12b3baed58339c5484e46a55266ea5cf6bb0
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-19 21:29:40 +0530

    ADDED:To line length equality

--- src/main/java/com/bridgelabz/LineComparison.java
+++ src/main/java/com/bridgelabz/LineComparison.java
@@ -13,10 +13,21 @@ public class LineComparison {
         int y1= input.nextInt();
         int y2= input.nextInt();
 
-        //Length of line calculation
-        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        System.out.println("To calculate line of length = "+ length);
+        int a1= input.nextInt();
+        int a2= input.nextInt();
+        int b1= input.nextInt();
+        int b2= input.nextInt();
 
+        //Length of line calculation
+        double lengthOne = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        double lengthTwo = Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2));
+
+        //Given equal operator
+        if (lengthOne == lengthTwo) {
+            System.out.println("Equal ");
+        } else {
+            System.out.println("Not Equal ");
+        }
 
 
     }
